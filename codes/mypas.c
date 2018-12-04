3ee79474dfa162aa1f5fc370a3b2a8761263dfc4
Lucas Atsuo Ito <lucasatsuo@gmail.com>
2018-12-04 15:08:01 -0200

    parametro hardcoded
    a aceitacao de parametros foi feita a mao

--- codes/mypas.c
+++ codes/mypas.c
@@ -7,9 +7,49 @@
 * 07/11/2018
 *
 */
+
+/*
+mypas input.pas [ -o output ]
+
+mypas input.pas
+ >> cria a.o
+
+mypas input.pas -o saida
+ >> cria saida.o
+
+*/
+
 #include <mypas.h>
 
-void main(void){
+void main(int argc, char *argv[]){
+	char **inputName, **outputName;
+	FILE *input, *output;
+
+	if(argc<2){
+		printf("Nao ha arquivos de entrada .. Finalizando\n");
+		exit(0);
+	}else{
+		inputName = &argv[1];
+		if(input = fopen(*inputName, "r") ){
+
+		}else{
+			printf("Nao foi possivel abrir %s .. Finalizando", *inputName);
+			exit(0);
+		}
+		if(strcmp(argv[2],"-o") == 0){
+			if(argc>3){
+				outputName = &argv[3];
+				if((output = fopen(strcat(*outputName,".o"), "w+"))==NULL){
+					printf("Erro ao criar o arquivo objeto .. Finalizando\n");
+					exit(0);
+				}
+			}
+		}else{
+		}
+	}
+
+
+
 	source = stdin;
 	object = stdout;
 	lookahead = gettoken(source);
